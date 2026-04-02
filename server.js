d5fbf6737e82959936da9b2b2b030f88d58d9c0d
sanyafox26 <sanyafox26@gmail.com>
2026-04-02 23:46:28 +0200

    Add Helmet.js for security headers

--- server.js
+++ server.js
@@ -62,7 +62,9 @@ const twilioClient = (process.env.TWILIO_ACCOUNT_SID && process.env.TWILIO_AUTH_
   ? twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
   : null;
 
+const helmet = require("helmet");
 const app = express();
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "12mb" }));
 // CORS for Capacitor + COOP for popups (Google Maps)
